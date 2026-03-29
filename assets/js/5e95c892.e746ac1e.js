574e6c098e6a905f846da4a656bf2cf306988ab2
salvamiguel <salvamiguel@users.noreply.github.com>
2026-03-29 13:49:20 +0000

    deploy: 1018368c5d46439400cd06c037b0ee6fe7bfd0ff

---
+++ assets/js/5e95c892.e746ac1e.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkmaterials=globalThis.webpackChunkmaterials||[]).push([[647],{7121(e,a,s){s.r(a),s.d(a,{default:()=>h});s(6540);var r=s(4164),l=s(7559),t=s(5500),u=s(2831),c=s(1656),i=s(4848);function h(e){return(0,i.jsx)(t.e3,{className:(0,r.A)(l.G.wrapper.docsPages),children:(0,i.jsx)(c.A,{children:(0,u.v)(e.route.routes)})})}}}]);
